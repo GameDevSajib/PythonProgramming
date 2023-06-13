73795d896f1c100e561aa5f2611b9559c8beb34b
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-06-13 04:54:14 -0700

    Add files via upload

---
+++ all_in_all.py
@@ -0,0 +1,43 @@
+import os
+
+##how to print
+print("hello")
+
+##how to take input 
+
+x=input('')
+
+###condition
+
+a=10
+b=20
+
+if a>b:
+    print(a, "is grather than", b)
+else:
+    print(b, "is grater than", a)
+
+##loop###
+for i in range(10):
+    print(i)
+
+
+
+
+
+###function###
+def sajib():
+    print("hello hacker")
+
+sajib()
+
+
+
+####loop with  initialization condition increment decrement
+
+for i in range(1,10,2):
+    print(i)
+
+
+    ###running unix commnad in python 
+os.system("firefox youtube.com")
